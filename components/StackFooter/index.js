984819527964ca7994c36c35a5a98724c404e72a
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-03-29 16:46:09 -0300

    feat: Added navigation support
    - navigation link
    - active link

--- components/StackFooter/index.js
+++ components/StackFooter/index.js
@@ -1,33 +1,53 @@
 import React from 'react'
+import { TabNavigator } from 'react-navigation'
 import {
+  Footer,
   FooterTab,
   Button,
   Icon,
-  Text,
+  Text
 } from 'native-base'
 
-const StackFooter = ({ navigation }) => {
-  return (
-    <FooterTab>
-      <Button
-        vertical
-        onPress={() => navigation.navigate('App')}
-      >
-        <Icon name="logo-buffer" />
-        <Text>Decks</Text>
-      </Button>
-      <Button
-        vertical
-        onPress={() => navigation.navigate('AddDeck')}
-      >
-        <Icon
-          ios="ios-add-circle"
-          android="md-add-circle"
-        />
-        <Text>New Deck</Text>
-      </Button>
-    </FooterTab>
-  )
-}
+import { DeckList } from '../Deck'
+import AddDeck from '../Deck/AddDeck'
+
+const StackFooter = TabNavigator(
+  {
+    DeckList,
+    AddDeck
+  },
+  {
+    tabBarPosition: 'bottom',
+    tabBarComponent: ({ navigationState, navigation }) => {
+      return (
+        <Footer>
+          <FooterTab>
+            <Button
+              vertical
+              active={navigationState.index === 0}
+              onPress={() => navigation.navigate('DeckList')}
+              style={{ backgroundColor: 'transparent' }}
+            >
+              <Icon name="logo-buffer" />
+              <Text>Decks</Text>
+            </Button>
+            <Button
+              vertical
+              active={navigationState.index === 1}
+              onPress={() => navigation.navigate('AddDeck')}
+              style={{ backgroundColor: 'transparent' }}
+            >
+              <Icon
+                ios="ios-add-circle"
+                android="md-add-circle"
+              />
+              <Text>New Deck</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      )
+    }
+  }
+)
 
 export default StackFooter
