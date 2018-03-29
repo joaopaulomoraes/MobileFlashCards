1c89cb367db27b03eec3cade2f0fb50bad21d909
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-03-29 11:44:39 -0300

    feat: Preparing app
    - Import fonts to Android support
    - Loader to app

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import Expo from 'expo'
+import { Container, Text } from 'native-base'
 
-export default class App extends React.Component {
+import StackFooter from './components/StackFooter'
+
+class App extends Component {
+  state = {
+    isReady: false
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("native-base/Fonts/Ionicons.ttf")
+    })
+    this.setState({ isReady: true })
+  }
   render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />
+    }
     return (
-      <View style={styles.container}>
-        <Text>Hello Flashcards</Text>
-      </View>
+      <Container>
+        <StackFooter />
+      </Container>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+export default App
