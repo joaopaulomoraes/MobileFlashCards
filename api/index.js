f26a870ce9374bd54791b1e026c4779ab257f1d6
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-04-02 09:20:34 -0300

    feat: Added api storage

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import { AsyncStorage } from 'react-native'
+
+export const FLASHCARDS_STORAGE_KEY = '@FlashCards:cards'
+
+export const sumitDeck = () => {
+  
+}
+
+export const removeDeck = () => {
+  
+}
