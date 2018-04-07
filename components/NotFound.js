dbd4c76db4750ce5480fe85fd696d754bb7a2b5a
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-04-07 12:15:21 -0300

    feat: Component NotFound created

---
+++ components/NotFound.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Dimensions
+} from 'react-native'
+import {
+  View,
+  Icon,
+  Text
+} from 'native-base'
+
+const { width, height } = Dimensions.get('window')
+
+const styles = StyleSheet.create({
+  content: {
+    padding: width / 12,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+const NotFound = ({ message }) => {
+  return (
+    <View style={styles.content}>
+      <Icon name="sad" />
+      <Text>No more decks here!</Text>
+    </View>
+  )
+}
+
+export default NotFound
