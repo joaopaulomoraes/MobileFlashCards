6da29cb12913917077b8466fc192f21ab8b15adc
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-04-11 19:42:28 -0300

    feat: Support to create add a deck

--- components/Deck/AddDeck.js
+++ components/Deck/AddDeck.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { StyleSheet } from 'react-native'
 import {
   Container,
@@ -16,51 +17,88 @@ import {
   Title,
   Body
 } from 'native-base'
+import {
+  Keyboard,
+  Alert,
+  Dimensions
+} from 'react-native'
+import { NavigationActions } from 'react-navigation'
+
+import { addDeck } from '../../actions'
+
+const { width, height } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    padding: 32,
+    padding: width / 12,
     flexDirection: 'column',
     justifyContent: 'center'
   }
 })
 
-const AddDeck = props => {
-  const { navigation } = props
-  return (
-    <Container>
-      <Header>
-        <Left>
-          <Button
-            transparent
-            onPress={() => navigation.goBack()}
-          >
-            <Icon name='arrow-back' />
-          </Button>
-        </Left>
-        <Body>
-          <Title>Add Deck</Title>
-        </Body>
-        <Right />
-      </Header>
-      <Content contentContainerStyle={styles.content}>
-        <H1 style={{textAlign: 'center'}}>What's the title of your new Deck?</H1>
-        <Form>
-          <Item>
-            <Input placeholder="Deck Title" />
-          </Item>
-          <Button
-            style={{marginTop: 62}}
-            block
-            primary
-          >
-            <Text>Add</Text>
-          </Button>
-        </Form>
-      </Content>
-    </Container>
-  )
+class AddDeck extends Component {
+  state = {
+    deckTitle: ''
+  }
+  addDeck = () => {
+    const { deckTitle } = this.state
+    deckTitle !== ''
+    ? this.props.addDeck( deckTitle )
+    : Alert.alert(
+      'Hey! 👋',
+      'The deck needs a name!'
+    )
+  }
+  render() {
+    const { navigation } = this.props
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => navigation.navigate('Home')}
+            >
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Add Deck</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content contentContainerStyle={styles.content}>
+          <H1 style={{textAlign: 'center'}}>What's the title of your new Deck?</H1>
+          <Form>
+            <Item>
+              <Input
+                autoFocus
+                placeholder="Deck title"
+                onChangeText={ deckTitle => this.setState({ deckTitle }) }
+              />
+            </Item>
+            <Button
+              style={{marginTop: 32}}
+              block
+              primary
+              onPress={() => this.addDeck()}
+              onSubmitEditing={Keyboard.dismiss}
+            >
+              <Text>Add</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    )
+  }
 }
 
-export default AddDeck
+mapDispatchToProps = dispatch => ({
+  addDeck: deckTitle => dispatch(addDeck(deckTitle))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(AddDeck)
